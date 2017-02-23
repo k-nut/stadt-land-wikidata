e02bea35bcfe5655c9d55689b53bccb193d7feaa
Knut Hühne <knut@k-nut.eu>
2017-02-23 19:21:10 +0100

    consistent naming of the app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1 class="ui header">Stadt, Land, Wikidata</h1>
+    <h1 class="ui header">Stadt, Land, Wikidata!</h1>
     <router-view></router-view>
   </div>
 </template>
